6c53363ae0d12dcf60d186960011a8756fa83ac2
Lascosck27 <gabriellascosck0909@gmail.com>
2023-09-25 23:00:46 -0300

    Update

--- Design de Classes/Program.cs
+++ Design de Classes/Program.cs
@@ -2,6 +2,9 @@
 using System.ComponentModel.DataAnnotations;
 using System.Security.Principal;
 
+/*Esse código cria duas instâncias: 1 de endereço e 1 de estudante.
+Depois de instanciar, as  mostra já formatadas no console.
+ */
 Endereco principal = new Endereco("Rua 26, Jabaeté", "Vila Velha", "Espírito Santo"); //instaciei um endereco novo
 Estudante Lascosck = new Estudante(52867, "Gabriel Lascosck de Oliveira", principal); //instanciei um estudante novo
 Lascosck.Display(); //chamei o mostrador formatado
